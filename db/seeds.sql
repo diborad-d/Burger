14b4e0ec5e4a77dddae7ca755f7fb44eba1425d4
Dibora D <diborad@yahoo.com>
2019-10-30 23:09:49 -0500

    set up api routes, views and handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('Whiskey King Burger','0',now(),now());
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('Chargrilled Burge','0',now(), now());
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('Le Pigeon Burger','0', now(), now());
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('Bohemian Radishy Burger','0', now(), now());
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('If looks Can Kale Burger', '0', now(), now());
+INSERT INTO burgers (Burger_name,Devoured, CreatedAt, UpdatedAt) VALUES ('Sweet Home Avocado Burger', '0', now(), now());
